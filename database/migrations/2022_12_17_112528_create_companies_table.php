e20405031dbe3ac3fcd3ebca19d3d19532a0a40c
abrar1107 <abrar@gigsfintech.com>
2022-12-17 17:03:25 +0530

    created company table

--- database/migrations/2022_12_17_112528_create_companies_table.php
+++ database/migrations/2022_12_17_112528_create_companies_table.php
@@ -15,6 +15,8 @@ class CreateCompaniesTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('company_name');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
